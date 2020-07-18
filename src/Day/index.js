cde179b0f7fc47a7264765dece5d57eb01ac2698
mohamed salamat <moahmed.salamat@gmail.com>
2020-07-18 20:16:00 +0100

    improve perf

--- src/Day/index.js
+++ src/Day/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {memo} from 'react';
 import styles from './Day.module.css';
 
 const Day = ({dayInfo}) => {
@@ -19,4 +19,4 @@ const Day = ({dayInfo}) => {
     )
 }
 
-export default Day;
+export default memo(Day);
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, memo} from 'react';
 import storeContext from '../storeManegement/storeContext';
 import TimeBlock from './TimeBlock';
 import ProgressBar from '../ProgressBar'
@@ -32,4 +32,4 @@ const Header = () => {
 }
 
 
-export default Header;
+export default memo(Header);
