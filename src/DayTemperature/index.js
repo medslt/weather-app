28e25586aef8927b6b84e2d6df012a810d42a959
mohamed salamat <moahmed.salamat@gmail.com>
2020-07-19 08:22:57 +0100

    animate temperature each 60 sec

--- src/DayTemperature/index.js
+++ src/DayTemperature/index.js
@@ -1,18 +1,20 @@
-import React, {useEffect, useRef, memo} from 'react';
+import React, {useEffect, useRef, memo, useContext} from 'react';
+import storeContext from '../storeManegement/storeContext';
 
 import styles from './DayTemperature.module.css';
 
 const DayTemperature = ({temperature}) => {
     const spanRef = useRef(null);
+    const { secondsToReload} = useContext(storeContext);
 
     useEffect(() => {
-        if (spanRef.current) {
+        if (secondsToReload === 0) {
             spanRef.current.style.fontSize = 'small'
             setTimeout(() => {
                 spanRef.current.style.fontSize = 'inherit'
             }, 300);
         }
-    }, [temperature])
+    }, [secondsToReload])
 
     return (
         <span ref={spanRef} className={styles.textAnimation}>
